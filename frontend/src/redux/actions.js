a5d3fd8e684e015614d71fd6dbfd183c1a29e4a6
Daniel Su <foeverdan@gmail.com>
2023-12-26 14:30:00 +1100

    config actions

---
+++ frontend/src/redux/actions.js
@@ -0,0 +1,18 @@
+// Action types
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+
+export const FETCH_DATA_REQUEST = 'FETCH_DATA_REQUEST';
+export const FETCH_DATA_SUCCESS = 'FETCH_DATA_SUCCESS';
+export const FETCH_DATA_FAILURE = 'FETCH_DATA_FAILURE';
+
+// Login actions
+export const loginRequest = (credentials) => ({ type: LOGIN_REQUEST, payload: credentials });
+export const loginSuccess = (token) => ({ type: LOGIN_SUCCESS, payload: token });
+export const loginFailure = (error) => ({ type: LOGIN_FAILURE, payload: error });
+
+// Data fetching actions
+export const fetchDataRequest = () => ({ type: FETCH_DATA_REQUEST });
+export const fetchDataSuccess = (data) => ({ type: FETCH_DATA_SUCCESS, payload: data });
+export const fetchDataFailure = (error) => ({ type: FETCH_DATA_FAILURE, payload: error });
