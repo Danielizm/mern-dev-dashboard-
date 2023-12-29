8f9d9541d72ef183b416cc931e1c39739baf48f8
Daniel Su <foeverdan@gmail.com>
2023-12-29 14:30:00 +1100

    Create login form component

---
+++ frontend/src/components/LoginForm.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { loginRequest } from '../redux/actions';
+
+const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+  const { loading, error } = useSelector((state) => state.auth);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(loginRequest({ email, password }));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Email:</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+      <button type="submit" disabled={loading}>Login</button>
+      {error && <p>{error}</p>}
+    </form>
+  );
+};
+
+export default LoginForm;
